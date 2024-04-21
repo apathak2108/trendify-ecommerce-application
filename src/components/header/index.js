f4737b56238b9d547300c645d2de3e91f5fe2ee1
Anany Pathak <propanany@gmail.com>
2024-04-21 23:26:46 +0530

    Loader Implementation for home page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import HeaderLogo from "../../images/trendify-logo.png";
 import { useNavigate } from "react-router-dom";
 import CartIcon from "../../images/cart-icon.png";
@@ -7,14 +7,11 @@ import "./Header.css";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-  const [dat, setDat] = useState();
+  const [hover, setHover] = useState(false);
   const mySet = useSelector(
     (state) => state?.cartItems?.numberOfCartItemsArray
   );
 
-  useEffect(() => {
-    setDat(mySet);
-  }, [mySet]);
   const cartCount = JSON.stringify([...mySet].length);
   const navigate = useNavigate();
   const handleLogout = () => {
@@ -24,39 +21,115 @@ const Header = () => {
   const handleHeaderLogo = () => {
     navigate("/home");
   };
+  const handleHover = () => {
+    setHover(true);
+  };
+  const handleHoverOut = () => {
+    setHover(false);
+  };
 
   return (
-    <div className="header">
-      <div className="header-left-portion">
-        <img
-          src={HeaderLogo}
-          className="header-logo"
-          alt="Logo"
-          onClick={handleHeaderLogo}
-        ></img>
-      </div>
-      <div className="header-right-portion">
-        <img
-          src={CartIcon}
-          className="cart-icon"
-          alt="Cart"
-          onClick={() => navigate("/checkout")}
-        />
-        {mySet.length !== 0 && (
-          <span
-            className="cart-checkout-items-value"
+    <>
+      <div className="header">
+        <div className="header-left-portion">
+          <img
+            src={HeaderLogo}
+            className="header-logo"
+            alt="Logo"
+            onClick={handleHeaderLogo}
+          ></img>
+        </div>
+        <div
+          className="navbar-pages"
+          onMouseOver={handleHover}
+          onMouseOut={handleHoverOut}
+        >
+          Women Ethnic
+        </div>
+        <div
+          className="navbar-pages"
+          onMouseOver={handleHover}
+          onMouseOut={handleHoverOut}
+        >
+          Women Western
+        </div>
+        <div
+          className="navbar-pages"
+          onMouseOver={handleHover}
+          onMouseOut={handleHoverOut}
+        >
+          Men
+        </div>
+        <div
+          className="navbar-pages"
+          onMouseOver={handleHover}
+          onMouseOut={handleHoverOut}
+        >
+          Kids
+        </div>
+        <div
+          className="navbar-pages"
+          onMouseOver={handleHover}
+          onMouseOut={handleHoverOut}
+        >
+          Home & Kitchen
+        </div>
+        <div
+          className="navbar-pages"
+          onMouseOver={handleHover}
+          onMouseOut={handleHoverOut}
+        >
+          Beauty & Health
+        </div>
+        <div
+          className="navbar-pages"
+          onMouseOver={handleHover}
+          onMouseOut={handleHoverOut}
+        >
+          Jewellery
+        </div>
+        <div
+          className="navbar-pages"
+          onMouseOver={handleHover}
+          onMouseOut={handleHoverOut}
+        >
+          Bags & Footwear
+        </div>
+        <div
+          className="navbar-pages"
+          onMouseOver={handleHover}
+          onMouseOut={handleHoverOut}
+        >
+          Electronics
+        </div>
+        <div className="header-right-portion">
+          <img
+            src={CartIcon}
+            className="cart-icon"
+            alt="Cart"
             onClick={() => navigate("/checkout")}
-          >
-            {cartCount}
-          </span>
-        )}
-        <Button
-          name="Logout"
-          onClick={handleLogout}
-          className="logout-button"
-        />
+          />
+          {mySet.length !== 0 && (
+            <span
+              className="cart-checkout-items-value"
+              onClick={() => navigate("/checkout")}
+            >
+              {cartCount}
+            </span>
+          )}
+          <Button
+            name="Logout"
+            onClick={handleLogout}
+            className="logout-button"
+          />
+        </div>
       </div>
-    </div>
+      {hover && (
+        <div className="on-hover-div">
+          <h1>Aur Baua</h1>
+        </div>
+      )}
+    </>
   );
 };
 
