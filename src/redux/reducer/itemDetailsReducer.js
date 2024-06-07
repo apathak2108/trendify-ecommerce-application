98e72208784939b105cd8ebac1e40e0fe08bbbc3
Anany Pathak <propanany@gmail.com>
2024-06-07 23:42:35 +0530

    changes made in routing

--- src/redux/reducer/itemDetailsReducer.js
+++ src/redux/reducer/itemDetailsReducer.js
@@ -2,6 +2,7 @@ import {
   IS_ANIMATION_TRIGGERED,
   ERROR_MESSAGE,
   NUMBER_OF_CART_ITEMS,
+  DELETE_CART_ITEM,
 } from "../action/actionTypes";
 
 const initialState = {
@@ -38,14 +39,20 @@ export const numberOfCartItemsReducer = (state = initialState, action) => {
   switch (action.type) {
     case NUMBER_OF_CART_ITEMS:
       return {
-
         ...state,
         numberOfCartItemsArray: state.numberOfCartItemsArray.add(
           action.payload
-        )
-
+        ),
+      };
+    case DELETE_CART_ITEM:
+      return {
+        ...state,
+        numberOfCartItemsReducer: state.numberOfCartItemsArray.splice(
+          action.payload,
+          1
+        ),
       };
     default:
-        return state;
+      return state;
   }
 };
